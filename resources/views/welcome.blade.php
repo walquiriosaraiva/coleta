8b68a6b30d3e0aa732cef76a7e25cd1acb1f9312
Walquirio Saraiva Rocha <walquiriosaraiva@gmail.com>
2022-02-15 20:00:29 -0300

    ajustes na ordenação de dados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,4 +70,3 @@
         });
     }
 </script>
-<script type="text/javascript">(function(n,r,l,d){try{var h=r.head||r.getElementsByTagName("head")[0],s=r.createElement("script");s.setAttribute("type","text/javascript");s.setAttribute("src",l);n.neuroleadId=d;h.appendChild(s);}catch(e){}})(window,document,"https://cdn.leadster.com.br/neurolead/neurolead.min.js", 44910);</script>
